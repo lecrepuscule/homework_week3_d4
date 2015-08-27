f3052db3a9bd8ec424f09740df304e9fa1b83d82
Yao Yuan <opal_obsidian@hotmail.com>
2015-08-27 22:03:28 +0100

    added continuous runProgramme until user decides to quit

--- mta.rb
+++ mta.rb
@@ -21,41 +21,52 @@ def getValidStation (line, mta)
   station = gets.chomp.split(" ").map{ |a| a.capitalize}.join(" ")
   readable_stations = getReadableStation(line, mta)
   while !(readable_stations.include?(station))
-    puts "Please choose from" + readable_stations.join(" , ")
+    puts "Please choose from " + readable_stations.join(" , ")
     station = gets.chomp.split(" ").map{ |a| a.capitalize}.join(" ")
   end
   return station
 end
 
-puts "Starting line N, L, 6? "
-start_line = getValidLine(mta)
-puts "start line is " + start_line.to_s
+def runProgramme (mta)
 
-puts "Starting Station? "
-start_station = getValidStation(start_line, mta)
-puts "start station is " + start_station.to_s
+  puts "Starting line N, L, 6? "
+  start_line = getValidLine(mta)
+  puts "start line is " + start_line.to_s
 
-puts "Destination line N, L, 6? "
-end_line = getValidLine(mta)
-puts "end line is " + end_line.to_s
+  puts "Starting Station? "
+  start_station = getValidStation(start_line, mta)
+  puts "start station is " + start_station.to_s
 
-puts "Destination Station? "
-end_station = getValidStation(end_line, mta)
-puts "end station is " + end_station.to_s
+  puts "Destination line N, L, 6? "
+  end_line = getValidLine(mta)
+  puts "end line is " + end_line.to_s
 
-start_stations = getReadableStation(start_line, mta)
-end_stations = getReadableStation(end_line, mta)
+  puts "Destination Station? "
+  end_station = getValidStation(end_line, mta)
+  puts "end station is " + end_station.to_s
 
-if start_line == end_line
-  stops = (start_stations.index(start_station) - start_stations.index(end_station)).abs
-else
-  intercept = mta[start_line] & mta[end_line]
-  stops1 = (start_stations.index(start_station) - start_stations.index(intercept[0])).abs
-  stops2 = (end_stations.index(end_station) - end_stations.index(intercept[0])).abs
-  stops = stops1 + stops2
+  start_stations = getReadableStation(start_line, mta)
+  end_stations = getReadableStation(end_line, mta)
+
+  if start_line == end_line
+    stops = (start_stations.index(start_station) - start_stations.index(end_station)).abs
+  else
+    intercept = mta[start_line] & mta[end_line]
+    stops1 = (start_stations.index(start_station) - start_stations.index(intercept[0])).abs
+    stops2 = (end_stations.index(end_station) - end_stations.index(intercept[0])).abs
+    stops = stops1 + stops2
+  end
+
+  puts "The number of stops are: " + stops.to_s
+  puts "Would you like to check another route? (y/n)"
+  safeword = gets.chomp.downcase
+  while safeword != "y" && safeword !="n"
+    puts "donno what you mean, please say 'y' or 'n'"
+    safeword = gets.chomp.downcase
+  end
+  safeword == "y" ? runProgramme(mta) : exit
 end
 
-puts "this number of stops are: " + stops.to_s
-
+runProgramme(mta)
 
   
